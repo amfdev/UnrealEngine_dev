af9a463fc11da21e8690f1c4578b9000efd949e0
ivm <ivm>
2018-05-04 17:49:19 +0300

    fix scripts

--- 4.17/Integration/BuildAllClean.bat
+++ 4.17/Integration/BuildAllClean.bat
@@ -7,14 +7,14 @@ IF ERRORLEVEL 1 GOTO :error
 @ECHO Prepare folders...
 SET UnrealHome=UnrealEngine-4.17
 SET AmfHome=AmfMedia-4.17
-goto test
+
 @ECHO Prepare UnrealEngine...
 IF NOT EXIST "%UnrealHome%" (
     @ECHO No UnrealEngine folder found, create it
     MKDIR "%UnrealHome%"
 ) ELSE (
     @ECHO UnrealEngine folder found, clear it
-    CALL 02-CleanUnrealEngine.bat
+    rem CALL 02-CleanUnrealEngine.bat
     IF ERRORLEVEL 1 GOTO :error
 )
     
@@ -42,13 +42,9 @@ IF ERRORLEVEL 1 GOTO :error
 CALL 05-BuildAmfLibraries.bat
 IF ERRORLEVEL 1 GOTO :error
 
-rem :test
-
-@ECHO Apply Amf libraries
+@ECHO Setup UnrealEngine
 CALL 07-SetupUnrealEngine.bat
 IF ERRORLEVEL 1 GOTO :error
-
-:test
 
 @ECHO Apply Amf libraries
 CALL 06-ApplyAmfLibraries.bat
@@ -56,6 +52,18 @@ IF ERRORLEVEL 1 (
     @ECHO ToDo: investigate why error returned here
     rem GOTO :error
 )
+
+@ECHO Prepare UnrealEngine solution
+CALL 07-PrepareUnrealEngineSolution.bat
+IF ERRORLEVEL 1 GOTO :error
+
+@ECHO Prepare UnrealEngine solution
+CALL 07-BuildUnrealEngine.bat
+IF ERRORLEVEL 1 GOTO :error
+
+@ECHO Build test scenes
+CALL 08-BuildScene.bat
+IF ERRORLEVEL 1 GOTO :error
 
 :done
     @ECHO Clean build finished!
--- 4.17/Integration/ResetGit.bat
+++ 4.17/Integration/ResetGit.bat
@@ -1,1 +1,15 @@
+@ECHO OFF
+SETLOCALE
+
+:choice
+set /P c=Are you sure you want hard reset you git repository? (Y/N) 
+if /I "%c%" EQU "Y" goto :yes
+if /I "%c%" EQU "N" goto :no
+goto :choice
+
+:no
+EXIT /B 0
+
+:yes
+@ECHO Hard reset git...
 git reset --hard && git clean -fdx
