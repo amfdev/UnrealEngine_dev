f841b95d718b625ddec14603bb9e4e0972bc39b5
ivm <ivm>
2018-05-14 17:56:03 +0300

    fix build all

--- integration/BuildAll.bat
+++ integration/BuildAll.bat
@@ -8,6 +8,10 @@ IF ERRORLEVEL 1 GOTO :error
 SET UE_VERSION=4.17
 SET AMF_VERSION=4.17
 
+@ECHO Build version 4.17
+SET UE_VERSION=4.17
+SET AMF_VERSION=4.17
+
 CALL 00-BuildAllImplementation.bat
 IF ERRORLEVEL 1 (
     @ECHO Error: failed to build version %UE_VERSION%
@@ -15,16 +19,18 @@ IF ERRORLEVEL 1 (
     @ECHO Build for version %UE_VERSION% successfull!
 )
 
-rem @ECHO Build version 4.18
-rem SET UE_VERSION=4.18
-rem SET AMF_VERSION=4.18
+exit /b 0
 
-rem CALL 00-BuildAllCleanImplementation.bat
-rem IF ERRORLEVEL 1 (
-rem    @ECHO Error: failed to build version %UE_VERSION%
-rem ) ELSE (
-rem     @ECHO Build for version %UE_VERSION% successfull!
-rem )
+@ECHO Build version 4.18
+SET UE_VERSION=4.18
+SET AMF_VERSION=4.18
+
+CALL 00-BuildAllImplementation.bat
+IF ERRORLEVEL 1 (
+    @ECHO Error: failed to build version %UE_VERSION%
+) ELSE (
+    @ECHO Build for version %UE_VERSION% successfull!
+)
 
 :done
     @ECHO Clean build finished
