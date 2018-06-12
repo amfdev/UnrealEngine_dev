a69f067bbb1961a020b9ca2eaac9a1b37cea9c80
ivm <ivm>
2018-06-12 16:13:18 +0300

    +x360 4.19

---
+++ integration/TestsProjects/4.19/x360AmfCpp/Source/x360AmfCpp/x360AmfCppGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "x360AmfCppGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class X360AMFCPP_API Ax360AmfCppGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
