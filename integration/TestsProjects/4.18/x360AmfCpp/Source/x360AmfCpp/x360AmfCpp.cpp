0841b620e596d8128baf6df884ccb6c1d9a42bb1
ivm <ivm>
2018-06-12 16:08:22 +0300

    +x360 4.18

---
+++ integration/TestsProjects/4.18/x360AmfCpp/Source/x360AmfCpp/x360AmfCpp.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "x360AmfCpp.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, x360AmfCpp, "x360AmfCpp" );
