da7af19d0c7e94ac7086723c934e6d0856d0539b
ivm <ivm>
2018-06-26 19:23:50 +0300

    + std x360 cpp

---
+++ integration/TestsProjects/4.18/x360StandardCpp/Source/x360StandardCpp/x360StandardCppGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "x360StandardCppGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class X360STANDARDCPP_API Ax360StandardCppGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
