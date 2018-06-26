1c37ce949df5cd6a14bcfebecf60492db71cfbf2
ivm <ivm>
2018-06-26 11:44:36 +0300

    fix

--- integration/Scripts/HelperClean.bat
+++ integration/Scripts/HelperClean.bat
@@ -6,16 +6,20 @@ IF NOT DEFINED PLUGIN_FOLDER (
     GOTO :error
 )
 
-CD %PLUGIN_FOLDER%
-IF ERRORLEVEL 1 GOTO :error
+IF EXIST "%PLUGIN_FOLDER%" (
 
-git reset --hard
-IF ERRORLEVEL 1 GOTO :error
+    CD %PLUGIN_FOLDER%
+    IF ERRORLEVEL 1 GOTO :error
 
-git clean -fdx
-IF ERRORLEVEL 1 GOTO :error
+    git reset --hard
+    IF ERRORLEVEL 1 GOTO :error
 
-RD /S /Q "%CD%/.git/rebase-apply"
+    git clean -fdx
+    IF ERRORLEVEL 1 GOTO :error
+
+    RD /S /Q "%CD%/.git/rebase-apply"
+
+)
 
 :done
     @ECHO Folder %PLUGIN_FOLDER% successfully cleaned
