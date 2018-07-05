9dee4687bab85a04e926c3e3c3ea56c59ca77310
ivm <ivm>
2018-07-05 18:52:29 +0300

    +common example

---
+++ integration/TestsProjects/4.18/MediaTest/Source/MediaTest/MediaTestGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MediaTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MEDIATEST_API AMediaTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
