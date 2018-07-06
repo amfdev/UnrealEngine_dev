ce8af75adf6235077aaef46baff7aab3d74f5870
ivm <ivm>
2018-07-06 11:20:48 +0300

    rename to support build system

---
+++ integration/TestsProjects/4.18/MediaTestAmfCpp/Source/MediaTestAmfCpp/MediaTestAmfCppGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MediaTestAmfCppGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MEDIATEST_API AMediaTestAmfCppGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
