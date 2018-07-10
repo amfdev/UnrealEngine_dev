f73be001a71019bbcca569cc1482265075bbff0e
ivm <ivm>
2018-07-10 12:59:17 +0300

    fixes

---
+++ integration/TestsProjects/4.18/MediaTestAmfCpp/Source/MediaTestAmfCpp/Private/MediaTestFunctionLibrary.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MediaTestFunctionLibrary.h"
+#include "Misc/FileHelper.h"
+#include "Misc/Paths.h"
+#include "Modules/ModuleManager.h"
+#include "Misc/PackageName.h"
+
+bool UMediaTestFunctionLibrary::FileSaveString(FString FileNameIn, FString SaveTextIn)
+{
+	                                 //*(FPaths::GameDir() + FileNameB)
+	return FFileHelper::SaveStringToFile(SaveTextIn, *FileNameIn);
+}
+
+bool UMediaTestFunctionLibrary::FileLoadString(FString FileNameIn, FString& SaveTextOut)
+{
+	return FFileHelper::LoadFileToString(SaveTextOut, *FileNameIn);
+}
+
+bool UMediaTestFunctionLibrary::FileLoadStringArray(FString FileNameIn, TArray<FString>& StringArrayOut)
+{
+	return FFileHelper::LoadFileToStringArray(StringArrayOut, *FileNameIn);
+}
+
+
+
+
