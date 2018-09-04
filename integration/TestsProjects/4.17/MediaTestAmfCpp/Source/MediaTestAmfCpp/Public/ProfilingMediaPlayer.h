b02f033f73b293a9adef85d673061155f4bbdc3b
ivm <ivm>
2018-09-04 12:04:04 +0300

    +Media player with profiler

---
+++ integration/TestsProjects/4.17/MediaTestAmfCpp/Source/MediaTestAmfCpp/Public/ProfilingMediaPlayer.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MediaPlayer.h"
+#include "ProfilingMediaPlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MEDIATESTAMFCPP_API UProfilingMediaPlayer : public UMediaPlayer
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
