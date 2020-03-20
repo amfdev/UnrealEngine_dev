771c2d0a83d1afb534166c379addf70095969796
iterator@inbox.ru <iterator@inbox.ru>
2020-03-20 16:40:49 +0300

    support unreal 4.23

--- integration/Build.bat
+++ integration/Build.bat
@@ -13,6 +13,7 @@ SET Build_4_19=
 SET Build_4_20=
 SET Build_4_21=
 SET Build_4_22=
+SET Build_4_23=
 SET Build_2015=
 SET Build_2017=
 SET Build_Amf=
@@ -71,6 +72,8 @@ FOR %%x IN (%*) DO (
         SET Build_4_21=1
     ) ELSE IF /I "%%~x"=="4.22" (
         SET Build_4_22=1
+    ) ELSE IF /I "%%~x"=="4.23" (
+        SET Build_4_23=1
     ) ELSE IF /I "%%~x"=="2015" (
         SET Build_2015=1
     ) ELSE IF /I "%%~x"=="2017" (
@@ -166,14 +169,15 @@ IF DEFINED Build_Verbose (
 
 @ECHO %Verbose%
 
-IF NOT DEFINED Build_4_17 IF NOT DEFINED Build_4_18 IF NOT DEFINED Build_4_19 IF NOT DEFINED Build_4_20 IF NOT DEFINED Build_4_21 IF NOT DEFINED Build_4_22 (
-    @ECHO No UnrealEngine version specified, 4.17, 4.18, 4.19, 4.20, 4.21, 4.22 will be added
+IF NOT DEFINED Build_4_17 IF NOT DEFINED Build_4_18 IF NOT DEFINED Build_4_19 IF NOT DEFINED Build_4_20 IF NOT DEFINED Build_4_21 IF NOT DEFINED Build_4_22 IF NOT DEFINED Build_4_23 (
+    @ECHO No UnrealEngine version specified, 4.17, 4.18, 4.19, 4.20, 4.21, 4.22, 4.23 will be added
     SET Build_4_17=1
     SET Build_4_18=1
     SET Build_4_19=1
     SET Build_4_20=1
     SET Build_4_21=1
     SET Build_4_22=1
+    SET Build_4_23=1
 )
 
 IF NOT DEFINED Build_2015 IF NOT DEFINED Build_2017 (
@@ -252,6 +256,8 @@ SET Build_4_19
 SET Build_4_20
 SET Build_4_21
 SET Build_4_22
+SET Build_4_23
+
 SET Build_2015
 SET Build_2017
 SET Build_Amf
@@ -310,7 +316,7 @@ FOR %%s IN (2015, 2017) DO (
     IF NOT DEFINED SkipVisualStudio (
         SET VS_VERSION=%%s
 
-        FOR %%v IN (17, 18, 19, 20, 21, 22) DO (
+        FOR %%v IN (17, 18, 19, 20, 21, 22, 23) DO (
 
             IF DEFINED Build_4_%%v (
                 CALL :runBuildHelper 4.%%v
@@ -331,7 +337,7 @@ FOR %%s IN (2015, 2017) DO (
     @ECHO Available commands:
     @ECHO     Engine - build Unreal Engine
     @ECHO     Tests - build tests
-    @ECHO     4.17 4.18 4.19 4.20 4.21 4.22 - specify Unreal Engine version
+    @ECHO     4.17 4.18 4.19 4.20 4.21 4.22 4.23 - specify Unreal Engine version
     @ECHO     2015 2017 - specify Visual Studio version
     @ECHO     Standard - build Unreal Engine and related tests with standard media playback
     @ECHO     Amf - build Unreal Engine and related tests with accelerated AMF media playback
--- integration/Series.bat
+++ integration/Series.bat
@@ -1,21 +1,27 @@
-call Build.bat 4.18 amf development origin SourcePatch
-call Build.bat 4.19 amf development origin SourcePatch
-call Build.bat 4.20 amf development origin SourcePatch
-call Build.bat 4.21 amf development origin SourcePatch
-call Build.bat 4.22 amf development origin SourcePatch
-call Build.bat 4.18 stitch development origin SourcePatch
-call Build.bat 4.19 stitch development origin SourcePatch
-call Build.bat 4.20 stitch development origin SourcePatch
-call Build.bat 4.21 stitch development origin SourcePatch
-call Build.bat 4.22 stitch development origin SourcePatch
+CALL Build.bat 4.18 amf development origin SourcePatch
+CALL Build.bat 4.19 amf development origin SourcePatch
+CALL Build.bat 4.20 amf development origin SourcePatch
+CALL Build.bat 4.21 amf development origin SourcePatch
+CALL Build.bat 4.22 amf development origin SourcePatch
+CALL Build.bat 4.23 amf development origin SourcePatch
 
-call Build.bat 4.18 amf shipping origin SourcePatch
-call Build.bat 4.19 amf shipping origin SourcePatch
-call Build.bat 4.20 amf shipping origin SourcePatch
-call Build.bat 4.21 amf shipping origin SourcePatch
-call Build.bat 4.22 amf shipping origin SourcePatch
-call Build.bat 4.18 stitch shipping origin SourcePatch
-call Build.bat 4.19 stitch shipping origin SourcePatch
-call Build.bat 4.20 stitch shipping origin SourcePatch
-call Build.bat 4.21 stitch shipping origin SourcePatch
-call Build.bat 4.22 stitch shipping origin SourcePatch
+CALL Build.bat 4.18 stitch development origin SourcePatch
+CALL Build.bat 4.19 stitch development origin SourcePatch
+CALL Build.bat 4.20 stitch development origin SourcePatch
+CALL Build.bat 4.21 stitch development origin SourcePatch
+CALL Build.bat 4.22 stitch development origin SourcePatch
+CALL Build.bat 4.23 stitch development origin SourcePatch
+
+CALL Build.bat 4.18 amf shipping origin SourcePatch
+CALL Build.bat 4.19 amf shipping origin SourcePatch
+CALL Build.bat 4.20 amf shipping origin SourcePatch
+CALL Build.bat 4.21 amf shipping origin SourcePatch
+CALL Build.bat 4.22 amf shipping origin SourcePatch
+CALL Build.bat 4.23 amf shipping origin SourcePatch
+
+CALL Build.bat 4.18 stitch shipping origin SourcePatch
+CALL Build.bat 4.19 stitch shipping origin SourcePatch
+CALL Build.bat 4.20 stitch shipping origin SourcePatch
+CALL Build.bat 4.21 stitch shipping origin SourcePatch
+CALL Build.bat 4.22 stitch shipping origin SourcePatch
+CALL Build.bat 4.23 stitch shipping origin SourcePatch
