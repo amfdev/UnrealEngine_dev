17a31df1b695cddaa98cb6e711297a5826bfe670
iterator@inbox.ru <iterator@inbox.ru>
2020-05-21 10:22:31 +0300

    support 4.24 version

--- integration/Build.bat
+++ integration/Build.bat
@@ -14,6 +14,7 @@ SET Build_4_20=
 SET Build_4_21=
 SET Build_4_22=
 SET Build_4_23=
+SET Build_4_24=
 SET Build_2015=
 SET Build_2017=
 SET Build_Amf=
@@ -74,6 +75,8 @@ FOR %%x IN (%*) DO (
         SET Build_4_22=1
     ) ELSE IF /I "%%~x"=="4.23" (
         SET Build_4_23=1
+    ) ELSE IF /I "%%~x"=="4.24" (
+        SET Build_4_24=1
     ) ELSE IF /I "%%~x"=="2015" (
         SET Build_2015=1
     ) ELSE IF /I "%%~x"=="2017" (
@@ -169,7 +172,7 @@ IF DEFINED Build_Verbose (
 
 @ECHO %Verbose%
 
-IF NOT DEFINED Build_4_17 IF NOT DEFINED Build_4_18 IF NOT DEFINED Build_4_19 IF NOT DEFINED Build_4_20 IF NOT DEFINED Build_4_21 IF NOT DEFINED Build_4_22 IF NOT DEFINED Build_4_23 (
+IF NOT DEFINED Build_4_17 IF NOT DEFINED Build_4_18 IF NOT DEFINED Build_4_19 IF NOT DEFINED Build_4_20 IF NOT DEFINED Build_4_21 IF NOT DEFINED Build_4_22 IF NOT DEFINED Build_4_23 IF NOT DEFINED Build_4_24 (
     @ECHO No UnrealEngine version specified, 4.17, 4.18, 4.19, 4.20, 4.21, 4.22, 4.23 will be added
     SET Build_4_17=1
     SET Build_4_18=1
@@ -178,6 +181,7 @@ IF NOT DEFINED Build_4_17 IF NOT DEFINED Build_4_18 IF NOT DEFINED Build_4_19 IF
     SET Build_4_21=1
     SET Build_4_22=1
     SET Build_4_23=1
+    SET Build_4_24=1
 )
 
 IF NOT DEFINED Build_2015 IF NOT DEFINED Build_2017 (
@@ -257,6 +261,7 @@ SET Build_4_20
 SET Build_4_21
 SET Build_4_22
 SET Build_4_23
+SET Build_4_24
 
 SET Build_2015
 SET Build_2017
@@ -316,7 +321,7 @@ FOR %%s IN (2015, 2017) DO (
     IF NOT DEFINED SkipVisualStudio (
         SET VS_VERSION=%%s
 
-        FOR %%v IN (17, 18, 19, 20, 21, 22, 23) DO (
+        FOR %%v IN (17, 18, 19, 20, 21, 22, 23, 24) DO (
 
             IF DEFINED Build_4_%%v (
                 CALL :runBuildHelper 4.%%v
--- integration/Series.bat
+++ integration/Series.bat
@@ -4,6 +4,7 @@ CALL Build.bat 4.20 amf development origin SourcePatch
 CALL Build.bat 4.21 amf development origin SourcePatch
 CALL Build.bat 4.22 amf development origin SourcePatch
 CALL Build.bat 4.23 amf development origin SourcePatch
+CALL Build.bat 4.24 amf development origin SourcePatch
 
 CALL Build.bat 4.18 stitch development origin SourcePatch
 CALL Build.bat 4.19 stitch development origin SourcePatch
@@ -11,6 +12,7 @@ CALL Build.bat 4.20 stitch development origin SourcePatch
 CALL Build.bat 4.21 stitch development origin SourcePatch
 CALL Build.bat 4.22 stitch development origin SourcePatch
 CALL Build.bat 4.23 stitch development origin SourcePatch
+CALL Build.bat 4.24 stitch development origin SourcePatch
 
 CALL Build.bat 4.18 amf shipping origin SourcePatch
 CALL Build.bat 4.19 amf shipping origin SourcePatch
@@ -18,6 +20,7 @@ CALL Build.bat 4.20 amf shipping origin SourcePatch
 CALL Build.bat 4.21 amf shipping origin SourcePatch
 CALL Build.bat 4.22 amf shipping origin SourcePatch
 CALL Build.bat 4.23 amf shipping origin SourcePatch
+CALL Build.bat 4.24 amf shipping origin SourcePatch
 
 CALL Build.bat 4.18 stitch shipping origin SourcePatch
 CALL Build.bat 4.19 stitch shipping origin SourcePatch
@@ -25,3 +28,4 @@ CALL Build.bat 4.20 stitch shipping origin SourcePatch
 CALL Build.bat 4.21 stitch shipping origin SourcePatch
 CALL Build.bat 4.22 stitch shipping origin SourcePatch
 CALL Build.bat 4.23 stitch shipping origin SourcePatch
+CALL Build.bat 4.24 stitch shipping origin SourcePatch
