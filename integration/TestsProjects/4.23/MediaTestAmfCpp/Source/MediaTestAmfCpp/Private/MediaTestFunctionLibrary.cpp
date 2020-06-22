f48eadf86ac0ec27a15dd9eee8f48a658cf0c44a
iterator@inbox.ru <iterator@inbox.ru>
2020-06-22 13:46:42 +0300

    + add 4.23 test prjects

---
+++ integration/TestsProjects/4.23/MediaTestAmfCpp/Source/MediaTestAmfCpp/Private/MediaTestFunctionLibrary.cpp
@@ -0,0 +1,403 @@
+#include "MediaTestFunctionLibrary.h"
+#include "Misc/FileHelper.h"
+#include "Misc/Paths.h"
+#include "Modules/ModuleManager.h"
+#include "Misc/PackageName.h"
+
+bool UMediaTestFunctionLibrary::FileSaveString(const FString& FileNameIn, const FString& SaveTextIn)
+{
+	                                 //*(FPaths::GameDir() + FileNameB)
+	return FFileHelper::SaveStringToFile(SaveTextIn, *FileNameIn);
+}
+
+bool UMediaTestFunctionLibrary::FileLoadString(const FString& FileNameIn, FString& SaveTextOut)
+{
+	return FFileHelper::LoadFileToString(SaveTextOut, *FileNameIn);
+}
+
+bool UMediaTestFunctionLibrary::FileLoadStringArray(const FString& FileNameIn, TArray<FString>& StringArrayOut)
+{
+	return LoadFileToStringArray(StringArrayOut, *FileNameIn);
+}
+
+bool UMediaTestFunctionLibrary::LoadFileToStringArray( TArray<FString>& Result, const TCHAR* Filename )
+{
+    Result.Empty();
+
+    FString Buffer;
+    if(!FFileHelper::LoadFileToString(Buffer, Filename))
+    {
+        return false;
+    }
+
+    for(const TCHAR* Pos = *Buffer; *Pos != 0; )
+    {
+        const TCHAR* LineStart = Pos;
+        while(*Pos != 0 && *Pos != '\r' && *Pos != '\n')
+        {
+            Pos++;
+        }
+
+        Result.Add(FString(Pos - LineStart, LineStart));
+
+        if(*Pos == '\r')
+        {
+            Pos++;
+        }
+        if(*Pos == '\n')
+        {
+            Pos++;
+        }
+    }
+
+    return true;
+}
+
+FString UMediaTestFunctionLibrary::GetCurrentPath()
+{
+	return FPaths::LaunchDir();
+}
+
+FString UMediaTestFunctionLibrary::GetRootDir()
+{
+	return FPaths::RootDir();
+}
+
+//FString UMediaTestFunctionLibrary::GetBaseDir()
+//{
+//	return FPaths::BaseDir();
+//}
+
+void UMediaTestFunctionLibrary::PrepareFullPath(const FString& BaseDirIn, const FString& FileNameIn, FString& FileNameOut)
+{
+	FileNameOut = FPaths::ConvertRelativePathToFull(BaseDirIn, FileNameIn);
+}
+
+bool UMediaTestFunctionLibrary::IsFileExist(const FString& FileNameIn)
+{
+	return FPaths::FileExists(FileNameIn);
+}
+
+bool UMediaTestFunctionLibrary::GrabOption(FString& Options, FString& Result)
+{
+	static FString QuestionMark(TEXT("?"));
+    static FString SwitchMark(TEXT("-"));
+    static FString QuotesMark(TEXT("\""));
+
+    FString WorkingCopy = TrimStart(Options);
+
+	int QuotesIndex = WorkingCopy.Find(QuotesMark, ESearchCase::CaseSensitive);
+    int CloseQoutesIndex = -1;
+
+    if (!QuotesIndex)
+    {
+        FString RightPart = Options.Right(Options.Len() - 1);
+        int CloseQoutesIndex = RightPart.Find(QuotesMark, ESearchCase::CaseSensitive);
+        
+        //Closing quotes found
+        if (-1 != CloseQoutesIndex)
+        {
+            WorkingCopy = RightPart.Left(CloseQoutesIndex);
+        }
+        //Error in syntax
+        else
+        {
+            return false;
+        }
+    }
+
+    int QuestionIndex = WorkingCopy.Find(QuestionMark, ESearchCase::CaseSensitive);
+    int SwitchIndex = WorkingCopy.Find(SwitchMark, ESearchCase::CaseSensitive);
+    bool GrabResult = false;
+
+    if (0 == QuestionIndex)
+    {
+        GrabResult = GrabParamEqualValue(WorkingCopy, Result);
+    }
+    else if (0 == SwitchIndex)
+    {
+        GrabResult = GrabLaunchOption(WorkingCopy, Result);
+    }
+
+    if (GrabResult)
+    {
+        //Error in syntax
+        if (!QuotesIndex && WorkingCopy.Trim().Len())
+        {
+            return false;
+        }
+
+        Options = WorkingCopy;
+
+        return true;
+    }
+
+    return false;
+}
+
+bool UMediaTestFunctionLibrary::GrabParamEqualValue(FString& Options, FString& ResultString)
+{
+    FString LaunchOption;
+    LaunchOption.Reserve(Options.Len());
+    
+    bool ParamFound = false;
+    bool ParamFinished = false;
+    bool ValueFound = false;
+    bool InQuotas = false;
+    
+    bool EndOption = false;
+    int Index = 0;
+
+    FString LaunchOptionAndValue;
+    LaunchOptionAndValue.Reserve(Options.Len());
+
+    for (Index = 1; !EndOption && Index < Options.Len(); ++Index)
+    {
+        TCHAR Char = Options[Index];
+
+        switch (Char)
+        {
+        case ' ':
+        case '\t':
+            if (!InQuotas)
+            {
+                // Don't allow extra spaces before param name, only ?param=value or ?param="value with spaces" allowed
+                if (!ParamFound)
+                {
+                    return false;
+                }
+                // Found end of the ?param=value sequence
+                else if (ValueFound)
+                {
+                    EndOption = true;
+
+                    break;
+                }
+                else
+                {
+                    // Don't allow extra spaces inside ?param=value sequence
+                    return false;
+                }
+            }
+            else
+            {
+                LaunchOptionAndValue.AppendChar(Char);
+            }
+
+            break;
+
+        case '\\':
+            // Don't allow escaping in the ?param= part
+            if (!InQuotas && !ParamFinished)
+            {
+                return false;
+            }
+            else
+            {
+                LaunchOptionAndValue.AppendChar(Char);
+            }
+
+            break;
+            
+        case '\'':
+        case '\"':
+            if (InQuotas)
+            {
+                // Test last symbol               Test space before other options
+                if ((Index == Options.Len() - 1) || (Options[Index + 1] == ' ') || (Options[Index + 1] == '\t'))
+                {
+                    InQuotas = false;
+                    EndOption = true;
+
+                    break;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (ParamFound && (Options[Index - 1] == '='))
+                {
+                    InQuotas = true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            break;
+
+        case '=':
+            if (ParamFound)
+            {
+                ParamFinished = true;
+                LaunchOptionAndValue.AppendChar(Char);
+            }
+            else
+            {
+                return false;
+            }
+
+            break;
+
+        default:
+            if (!ParamFound)
+            {
+                ParamFound = true;
+            }
+            else if (LaunchOptionAndValue[LaunchOptionAndValue.Len() - 1] == '=')
+            {
+                ValueFound = true;
+            }
+
+            LaunchOptionAndValue.AppendChar(Char);
+        }
+
+        if (!InQuotas && ParamFound && ValueFound && (Index == Options.Len() - 1))
+        {
+            EndOption = true;
+        }
+    }
+
+    if (LaunchOptionAndValue.Len() && EndOption)
+    {
+        ResultString = LaunchOptionAndValue;
+        Options = Options.Right(Options.Len() - Index);
+
+        return true;
+    }
+
+    return false;
+}
+
+bool UMediaTestFunctionLibrary::GrabLaunchOption(FString& Options, FString& ResultString)
+{
+    int Index = 0;
+
+    FString LaunchOption;
+    LaunchOption.Reserve(Options.Len());
+    bool SkipDash = false;
+    bool Break = false;
+
+    for (Index = 1; !Break && (Index < Options.Len()); ++Index)
+    {
+        TCHAR Char = Options[Index];
+
+        switch (Char)
+        {
+        // Skip first dash
+        case '-':
+            if (!SkipDash)
+            {
+                SkipDash = true;
+
+                break;
+            }
+
+            // Don't allow --LaunchOptionName
+            return false;
+
+        case L' ':
+        case L'\t':
+            Break = true;
+
+            break;
+
+        // Don't allow escaping in the launch option
+        case '\\':
+            return false;
+
+        // Don't allow quoted text in the launch option
+        case '\'':
+        case '\"':
+            return false;
+
+        default:
+            LaunchOption.AppendChar(Char);
+        }
+    }
+
+    if (LaunchOption.Len())
+    {
+        ResultString = LaunchOption;
+        Options = Options.Right(Options.Len() - Index);
+    }
+
+    return LaunchOption.Len() > 0;
+}
+
+FString UMediaTestFunctionLibrary::TrimStart(const FString& String)
+{
+    FString Result(String);
+    int32 Pos = 0;
+    while(Pos < Result.Len() && FChar::IsWhitespace(Result[Pos]))
+    {
+        Pos++;
+    }
+    Result.RemoveAt(0, Pos);
+
+    return Result;
+}
+
+void UMediaTestFunctionLibrary::GetKeyValue(const FString& Pair, FString& Key, FString& Value)
+{
+    const int32 EqualSignIndex = Pair.Find(TEXT("="), ESearchCase::CaseSensitive);
+	if (EqualSignIndex != INDEX_NONE)
+	{
+		Key = Pair.Left(EqualSignIndex);
+		Value = Pair.Mid(EqualSignIndex + 1, MAX_int32);
+	}
+	else
+	{
+		Key = Pair;
+		Value = TEXT("");
+	}
+}
+
+bool UMediaTestFunctionLibrary::HasOption(FString Options, const FString& Key)
+{
+	FString Pair, PairKey, PairValue;
+	while (GrabOption(Options, Pair))
+	{
+		GetKeyValue(Pair, PairKey, PairValue);
+		if (Key == PairKey)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+FString UMediaTestFunctionLibrary::ParseOption(FString Options, const FString& Key)
+{
+	FString ReturnValue;
+	FString Pair, PairKey, PairValue;
+	while (GrabOption(Options, Pair))
+	{
+		GetKeyValue(Pair, PairKey, PairValue);
+		if (!Key.Compare(PairKey, ESearchCase::IgnoreCase))
+		{
+			ReturnValue = MoveTemp(PairValue);
+			break;
+		}
+	}
+	return ReturnValue;
+}
+
+bool UMediaTestFunctionLibrary::HasLaunchOption(FString Options, const FString& OptionToCheck)
+{
+	bool ReturnValue = false;
+	FString Grabbed;
+	while (GrabOption(Options, Grabbed))
+	{
+		if (!OptionToCheck.Compare(Grabbed, ESearchCase::IgnoreCase))
+		{
+			ReturnValue = true;
+			break;
+		}
+	}
+	return ReturnValue;
+}
---
+++ integration/TestsProjects/4.23/MediaTestAmfCpp/Source/MediaTestAmfCpp/Public/MediaTestFunctionLibrary.h
@@ -0,0 +1,93 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Misc/FileHelper.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "MediaTestFunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MEDIATESTAMFCPP_API UMediaTestFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+	UFUNCTION(BlueprintCallable, Category = "Files")
+	static bool FileSaveString(const FString& FileNameIn, const FString& SaveTextIn);
+
+	UFUNCTION(BlueprintPure, Category = "Files")
+	static bool FileLoadString(const FString& FileNameIn, FString& SaveTextOut);
+
+	UFUNCTION(BlueprintPure, Category = "Files")
+	static bool FileLoadStringArray(const FString& FileNameIn, TArray<FString>& StringArrayOut);
+
+    /**
+    * Load a text file to an array of strings. Supports all combination of ANSI/Unicode files and platforms.
+    *
+    * @param Result       String representation of the loaded file
+    * @param Filename     Name of the file to load
+    * @param VerifyFlags  Flags controlling the hash verification behavior ( see EHashOptions )
+    */
+    static bool LoadFileToStringArray( TArray<FString>& Result, const TCHAR* Filename );
+
+	UFUNCTION(BlueprintPure, Category = "System")
+	static FString GetCurrentPath();
+
+	UFUNCTION(BlueprintPure, Category = "System")
+	static FString GetRootDir();
+
+	//UFUNCTION(BlueprintPure, Category = "System")
+	//static FString GetBaseDir();
+
+	UFUNCTION(BlueprintPure, Category = "System")
+	static void PrepareFullPath(const FString& BaseDirIn, const FString& FileNameIn, FString& FileNameOut);
+
+	UFUNCTION(BlueprintPure, Category = "System")
+	static bool IsFileExist(const FString& FileNameIn);
+
+	static bool GrabOption(FString& Options, FString& ResultString);
+    static bool GrabParamEqualValue(FString& Options, FString& ResultString);
+    static bool GrabLaunchOption(FString& Options, FString& ResultString);
+
+    /**
+    * Removes whitespace characters from the start of this string.
+    * @note Unlike Trim() this function returns a copy, and does not mutate the string.
+    */
+    UFUNCTION(BlueprintPure, Category = "System")
+    static FString TrimStart(const FString& String);
+
+    /**
+	* Break up a key=value pair into its key and value.
+	* @param Pair			The string containing a pair to split apart.
+	* @param Key			(out) Key portion of Pair. If no = in string will be the same as Pair.
+	* @param Value			(out) Value portion of Pair. If no = in string will be empty.
+	*/
+	UFUNCTION(BlueprintPure, Category = "System", meta = (BlueprintThreadSafe))
+	static void GetKeyValue(const FString& Pair, FString& Key, FString& Value);
+
+	/**
+	* Returns whether a key exists in an options string.
+	* @param Options		The string containing the options.
+	* @param Key			The key to determine if it exists in Options.
+	* @return				Whether Key was found in Options.
+	*/
+	UFUNCTION(BlueprintPure, Category = "System", meta = (BlueprintThreadSafe))
+	static bool HasOption(FString Options, const FString& InKey);
+
+	/**
+	* Find an option in the options string and return it.
+	* @param Options		The string containing the options.
+	* @param Key			The key to find the value of in Options.
+	* @return				The value associated with Key if Key found in Options string.
+	*/
+	UFUNCTION(BlueprintPure, Category = "System", meta = (BlueprintThreadSafe))
+	static FString ParseOption(FString Options, const FString& Key);
+
+	/**
+	* Checks the commandline to see if the desired option was specified on the commandline (e.g. -demobuild)
+	* @return				True if the launch option was specified on the commandline, false otherwise
+	*/
+	UFUNCTION(BlueprintPure, Category = "System")
+	static bool HasLaunchOption(FString Options, const FString& OptionToCheck);
+};
