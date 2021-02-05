b642f5dd28f8edb2304a1efea09d0f833651995e
iterator@inbox.ru <iterator@inbox.ru>
2021-02-05 13:32:30 +0300

    + test app for 4.26

---
+++ integration/TestsProjects/4.26/MediaTestAmfCpp/Source/MediaTestAmfCpp/MediaTestAmfCpp.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MediaTestAmfCpp.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MediaTestAmfCpp, "MediaTestAmfCpp" );
